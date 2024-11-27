4390f4ec496b1e114d7c960d0bc34ed72a4436d1
Andhika Akbar Pratama <q4quekar@gmail.com>
2024-11-27 22:26:53 +0700

    Update test2.cpp
    menambah loading

--- test2.cpp
+++ test2.cpp
@@ -6,59 +6,95 @@
 #include <string>
 using namespace std;
 
-void Koor(int x, int y) {
+void KOOR(int x, int y) {
     COORD coord;
     coord.X = x;
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void ArtRegister() {
-    Koor(20, 5);  cout << "#####   ######  ##### ####  #### ###### ###### #####   ";
-    Koor(20, 6);  cout << " ## ##   ##  # ##  ##  ##  ##  # # ## #  ##  #  ## ##  ";
-    Koor(20, 7);  cout << " ## ##   ##    ##      ##  ###     ##    ##     ## ##  ";
-    Koor(20, 8);  cout << " ####    ###   ## ###  ##   ###    ##    ###    ####   ";
-    Koor(20, 9);  cout << " ## #    ##    ##  ##  ##    ###   ##    ##     ## #   ";
-    Koor(20, 10); cout << " ## ##   ##  # ##  ##  ##  #  ##   ##    ##  #  ## ##  ";
-    Koor(20, 11); cout << "### ### ######  ####  #### ####   ####  ###### ### ### ";
+void Hello(){
+    cout << "\e[33m";
+    KOOR(33, 10); cout <<    " ____  ____   ________   _____      _____        ___    ";
+    Sleep(100);
+    KOOR(33, 11); cout <<    "|_   ||   _| |_   __  | |_   _|    |_   _|     .'   `.  ";
+    Sleep(100);
+    KOOR(33, 12); cout <<   "  | |__| |     | |_ \\_|   | |        | |      /  .-.  \\ ";
+    Sleep(100);
+    KOOR(33, 13); cout <<    "  |  __  |     |  _| _    | |   _    | |   _  | |   | | ";
+    Sleep(100);
+    KOOR(33, 14); cout <<    " _| |  | |_   _| |__/ |  _| |__/ |  _| |__/ | \\  `-'  / ";
+    Sleep(100);
+    KOOR(33, 15); cout <<    "|____||____| |________| |________| |________|  `.___.'  ";
+    Sleep(3000);
+    cout << "\e[0m";
+}
+void loading(){
+    mvprintw(15, 70, "Loading...");
+        mvprintw(16, 60, "-----------------------------");
+        mvprintw(17, 59, "|                             |");
+        mvprintw(18, 60, "-----------------------------");
+
+    for(int j = 0; j < 29; j++){
+        attron(COLOR_PAIR(2));
+        mvprintw(17, 60 + j, " ");
+        refresh();
+        Sleep(100);
+    }
 }
 
-void ArtLogin() {
-    Koor(20, 5);  cout << "####    ####   ##### #### ### ### ";
-    Koor(20, 6);  cout << " ##    ##  ## ##  ##  ##   ##  #  ";
-    Koor(20, 7);  cout << " ##    ##  ## ##      ##   ### #  ";
-    Koor(20, 8);  cout << " ##    ##  ## ## ###  ##   # # #  ";
-    Koor(20, 9);  cout << " ##  # ##  ## ##  ##  ##   # ###  ";
-    Koor(20, 10); cout << " ## ## ##  ## ##  ##  ##   #  ##  ";
-    Koor(20, 11); cout << "######  ####   ####  #### ###  #  ";
+void RegisART() {
+    cout << "\e[33m";
+    KOOR(20, 5);  cout << "#####   ######  ##### ####  #### ###### ###### #####   ";
+    KOOR(20, 6);  cout << " ## ##   ##  # ##  ##  ##  ##  # # ## #  ##  #  ## ##  ";
+    KOOR(20, 7);  cout << " ## ##   ##    ##      ##  ###     ##    ##     ## ##  ";
+    KOOR(20, 8);  cout << " ####    ###   ## ###  ##   ###    ##    ###    ####   ";
+    KOOR(20, 9);  cout << " ## #    ##    ##  ##  ##    ###   ##    ##     ## #   ";
+    KOOR(20, 10); cout << " ## ##   ##  # ##  ##  ##  #  ##   ##    ##  #  ## ##  ";
+    KOOR(20, 11); cout << "### ### ######  ####  #### ####   ####  ###### ### ### ";
+    cout << "\e[0m";
+}
+
+void LoginART() {
+    cout << "\e[33m";
+    KOOR(20, 5);  cout << "####    ####   ##### #### ### ### ";
+    KOOR(20, 6);  cout << " ##    ##  ## ##  ##  ##   ##  #  ";
+    KOOR(20, 7);  cout << " ##    ##  ## ##      ##   ### #  ";
+    KOOR(20, 8);  cout << " ##    ##  ## ## ###  ##   # # #  ";
+    KOOR(20, 9);  cout << " ##  # ##  ## ##  ##  ##   # ###  ";
+    KOOR(20, 10); cout << " ## ## ##  ## ##  ##  ##   #  ##  ";
+    KOOR(20, 11); cout << "######  ####   ####  #### ###  #  ";
+    cout << "\e[0m";
 }
 
 
 void registration() {
+	curs_set(1);
     ofstream file("userdata.txt", ios::app);
     string username;
     system("cls");
-	ArtRegister();
-    Koor(20, 13); cout << "Register username : ";
-    std::cin >> username;
+	RegisART();
+    KOOR(20, 13); cout << "Enter a username to register : ";
+    cin >> username;
     if (file.is_open()) {
         file << username << endl;
-        Koor(20,15);cout << "Registration successful!";
+        KOOR(20,15);cout << "Registration successful!";
     } else {
-        Koor(20,15);cout << "Registration failed!";
+        KOOR(20,15);cout << "Error opening file!";
     }
     file.close();
-    Koor(20,17);cout << "Please wait";
-    Sleep(4000);
+    KOOR(20,17);cout << "Please wait";
+    Sleep(5000);
 }
 
 void login() {
+	curs_set(1);
     ifstream file("userdata.txt");
     string username, line;
     bool login_success = false;
     system("cls");
-	ArtLogin();
-    Koor(20, 13); cout << "Enter username : ";
+    LoginART();
+    KOOR(20, 13); cout << "Enter your username : ";
     cin >> username;
 
     if (file.is_open()) {
@@ -72,18 +108,22 @@ void login() {
     }
 
     if (login_success) {
-        Koor(20,15);cout << "Login successful!";
+        KOOR(20,15);cout << "Login successful!";
     } else {
-        Koor(20,15);cout << "Username not found";
+        KOOR(20,15);cout << "Username not found";
     }
-    Koor(20,17);cout << "Please wait";
-    Sleep(4000);
+    KOOR(20,17);cout << "Please wait";
+    Sleep(5000);
 }
 
 int main() {
+    system("cls");
+    Hello();
     initscr();
     start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_YELLOW);
+    loading();
     bkgd(COLOR_PAIR(1));
     noecho();
     curs_set(0);
@@ -111,8 +151,10 @@ int main() {
         } else if (key == '\n') {
             endwin();
             if (choice == 0) {
+            	    curs_set(1);
                 registration();
             } else if (choice == 1) {
+            	    curs_set(1);
                 login();
             } else if (choice == 2) {
                 break;
