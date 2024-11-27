d3105639b9e0090a15b623053f6c2540cd88058d
Zahirasafa <raraadiah6@gmail.com>
2024-11-27 23:35:09 +0700

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -1,1 +1,33 @@
+#include <ncurses/ncurses.h>
+#include <unistd.h>
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void KOOR(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void Hello(){
+    cout << "\e[33m";
+    KOOR(33, 10); cout <<    " ____  ____   ________   _____      _____        ___    ";
+    Sleep(100);
+    KOOR(33, 11); cout <<    "|_   ||   _| |_   __  | |_   _|    |_   _|     .'   `.  ";
+    Sleep(100);
+    KOOR(33, 12); cout <<   "  | |__| |     | |_ \\_|   | |        | |      /  .-.  \\ ";
+    Sleep(100);
+    KOOR(33, 13); cout <<    "  |  __  |     |  _| _    | |   _    | |   _  | |   | | ";
+    Sleep(100);
+    KOOR(33, 14); cout <<    " _| |  | |_   _| |__/ |  _| |__/ |  _| |__/ | \\  `-'  / ";
+    Sleep(100);
+    KOOR(33, 15); cout <<    "|____||____| |________| |________| |________|  `.___.'  ";
+    Sleep(3000);
+    cout << "\e[0m";
+    system("cls");
+}
+
 
