e1f26176b18089fe1b1197cf45f3303a140da1eb
Andhika Akbar Pratama <q4quekar@gmail.com>
2024-11-27 18:36:58 +0700

    Update test2.cpp
    menambah fungsi koordinat

--- test2.cpp
+++ test2.cpp
@@ -1,3 +1,10 @@
 #include <ncurses/ncurses.h>
 #include <iostream>
+using namespace std;
 
+void koordinat(int x, int y){
+  COORD coord;
+  coord.X = x;
+  coord.Y = y;
+  SetConsoleCursorPosition(GetSdHandle(STD_OUTPUT_HANDLE), coord);
+}
