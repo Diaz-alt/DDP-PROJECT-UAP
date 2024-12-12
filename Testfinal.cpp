d61fa6b0cc388f3155afec4e421f5fc6e8073be8
Zahirasafa <raraadiah6@gmail.com>
2024-12-12 12:02:28 +0700

    Update Testfinal.cpp

--- Testfinal.cpp
+++ Testfinal.cpp
@@ -165,65 +165,6 @@ const char hard[21][30] = {
     {'P', 'r', 'e', 's', 's', ' ', 'E', 's', 'c', '=', 'B', 'a', 'c', 'k', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
 };
 
-void gamemode(){
-    system("cls");
-    initscr();
-    keypad(stdscr, TRUE);
-    noecho();
-    curs_set(0);
-    const char* options[] = { "Easy", "Normal", "Hard", "Cancel"};
-    int choice = 0;
-    int key;
-    while (true) {
-        clear();
-        for (int i = 0; i < 4; i++) {
-            if (i == choice)
-                attron(A_REVERSE);
-            mvprintw(7 + i, 12, options[i]);
-            attroff(A_REVERSE);
-        }
-        key = getch();
-        
-        if (key == KEY_DOWN) {
-            choice = (choice + 1) % 4;
-        } else if (key == KEY_UP) {
-            choice = (choice + 3) % 4;
-        } else if (key == 10) {
-            if(choice == 0){
-                noecho();
-                curs_set(1);
-                curs_set(0);
-                maze(easy);
-            movePlayer(easy);
-            break;
-            } else if(choice == 1) {
-                noecho();
-                curs_set(1);
-                curs_set(0);
-                maze(medium);
-            movePlayer(medium);
-            break;
-            } else if(choice == 2) {
-                noecho();
-                curs_set(1);
-            	curs_set(0);
-                maze(hard);
-            movePlayer(hard);
-            break;
-            } else if(choice == 3) {
-                system("cls");
-                KOOR(20, 9);    cout << "==============";
-                KOOR(20, 10);   cout << "Please wait...";
-                KOOR(20, 11);   cout << "==============";
-                Sleep(2000);
-                system("cls");
-                break;
-            }
-            curs_set(0);
-            noecho();
-        }
-    }
-}
 
 void registration() {
     curs_set(1);
